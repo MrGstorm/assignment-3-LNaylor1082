456c726b5ddd5a1c07908c5f89a1f35f1fafbfde
LNaylor1082 <lnaylor1082@hwdsb.on.ca>
2025-10-02 08:38:27 -0400

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,12 @@
+###############################################################################
+#  Program Name   : ex8.py
+#  Author         : Liam Naylor
+#  Task           : User inputs as many words as they want until they input 'exit'
+###############################################################################
+
+# Ask the user to input words until they type exit, which ends the program
+
+while True:
+    word = input("GIMME SOME WORDSSSSSSSS (type 'exit' to end): ")
+    if word == "exit":
+        break
