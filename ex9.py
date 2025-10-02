aff825491a2e804a4def1e3b602c38a28cf449b1
LNaylor1082 <lnaylor1082@hwdsb.on.ca>
2025-10-02 08:38:55 -0400

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,10 @@
+###############################################################################
+#  Program Name   : ex9.py
+#  Author         : Liam Naylor
+#  Task           : Print every odd number from 1-19 in a straight line
+###############################################################################
+
+# Print the numbers from 1-20, but skip every even number
+
+for i in range(1, 20, 2):
+    print(i)
