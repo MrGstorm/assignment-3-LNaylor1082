50e3d034de03491165dcb9f5f7f3f64becd4dbde
LNaylor1082 <lnaylor1082@hwdsb.on.ca>
2025-10-07 08:55:25 -0400

    Create errors.py

---
+++ errors.py
@@ -0,0 +1,79 @@
+###############################################################################
+#  Program Name   : errors.py
+#  Author         : Liam Naylor
+#  Task           : A huge list showing examples of coding errors
+###############################################################################
+
+# SyntaxError
+
+# print('henlo wrld")
+
+# IndentationError
+
+# num1 = 3
+# num2 = 6
+# if num1 > num2:
+# print("neembar juan esss graterrr")
+
+# NameError
+
+# print(color, "is my favourite cooleeer!")
+
+# TypeError
+
+# a = "three"
+# b = 9
+# print(a + b)
+
+# ValueError
+
+# x = float("henlo")
+
+# ZeroDivisionError
+
+# a = 5
+# b = 0
+# print(a/b)
+
+# IndexError
+
+# aList = [1, 2, 3, 4]
+# print(aList[10])
+
+# KeyError
+
+# a = {'abc': 3}
+# print(a['bcd'])
+
+# AttributeError
+
+# X = 10
+# X.append(6)
+
+# ImportError
+
+# from random import Pizza
+
+# ModuleNotFoundError
+
+# import something_that_does_not_exist_module
+
+# FileNotFoundError
+
+# file = open("abc.txt")
+
+# RuntimeError
+
+# raise RuntimeError
+
+# RecursionError
+
+# def tooMany():
+    # print("RecursionErrorRAHHHHHHH")
+    # tooMany()
+
+# tooMany()
+
+# OverflowError
+
+# print(10.0 ** 1000)
